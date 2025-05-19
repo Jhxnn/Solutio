34d40cf9720d48b45e6c86365812074d654e1ac4
Hilixx <jhonalizan@gmail.com>
2025-05-19 14:03:24 -0300

    first

---
+++ src/main/java/com/Solutio/SolutioApplication.java
@@ -0,0 +1,13 @@
+package com.Solutio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Solutio/SolutioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Solutio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
