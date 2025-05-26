11ffc94f98aec8880c842cfcf97eccf94051aef3
Hilixx <jhonalizan@gmail.com>
2025-05-26 08:21:06 -0300

    config: customerDto

--- src/main/java/com/Solutio/Dtos/CustomerDto.java
+++ src/main/java/com/Solutio/Dtos/CustomerDto.java
@@ -1,9 +1,13 @@
 package com.Solutio.Dtos;
 
+import com.Solutio.Models.Enums.Role;
+
 public record CustomerDto(
         String name,
         String email,
+        String password,
         String cpfCnpj,
         String phone,
-        String address) {
+        String address,
+        Role role) {
 }
