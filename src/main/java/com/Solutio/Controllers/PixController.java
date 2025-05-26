74c901ddbbc03691a87aaa54cdbac386715e2d78
Hilixx <jhonalizan@gmail.com>
2025-05-26 14:41:41 -0300

    feature: pixTransaction controller

--- src/main/java/com/Solutio/Controllers/PixController.java
+++ src/main/java/com/Solutio/Controllers/PixController.java
@@ -1,16 +1,19 @@
 package com.Solutio.Controllers;
 
 
+import com.Solutio.Dtos.PixTransaction;
 import com.Solutio.Models.Pix;
 import com.Solutio.Services.PixService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/pix")
@@ -23,6 +26,11 @@ public class PixController {
     @GetMapping
     public ResponseEntity<List<Pix>> findALl(){
         return ResponseEntity.status(HttpStatus.OK).body(pixService.findAll());
+    }
+
+    @GetMapping("/qrCode/{pixId}")
+    public ResponseEntity<PixTransaction> pixTransaction(@PathVariable(name = "pixId")UUID pixId){
+        return ResponseEntity.status(HttpStatus.OK).body(pixService.getPixTransaction(pixId));
     }
 
 }
