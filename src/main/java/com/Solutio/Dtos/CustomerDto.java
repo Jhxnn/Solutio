63ec932d56667241228111e521be1e5d33e365d0
Hilixx <jhonalizan@gmail.com>
2025-06-02 11:28:24 -0300

    config: removing password and role from customerDto

--- src/main/java/com/Solutio/Dtos/CustomerDto.java
+++ src/main/java/com/Solutio/Dtos/CustomerDto.java
@@ -5,9 +5,7 @@ import com.Solutio.Models.Enums.Role;
 public record CustomerDto(
         String name,
         String email,
-        String password,
         String cpfCnpj,
         String phone,
-        String address,
-        Role role) {
+        String address) {
 }
