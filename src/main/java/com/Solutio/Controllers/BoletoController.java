bdf8d5c7562afcbacc240c2b892f9760a0093ed2
Hilixx <jhonalizan@gmail.com>
2025-06-02 11:16:28 -0300

    feature: findBoletoCharge

--- src/main/java/com/Solutio/Controllers/BoletoController.java
+++ src/main/java/com/Solutio/Controllers/BoletoController.java
@@ -2,6 +2,7 @@ package com.Solutio.Controllers;
 
 import com.Solutio.Models.Boleto;
 import com.Solutio.Services.BoletoService;
+import com.Solutio.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -19,8 +20,16 @@ public class BoletoController {
     @Autowired
     BoletoService boletoService;
 
+    @Autowired
+    UserService userService;
+
     @GetMapping
     public ResponseEntity<List<Boleto>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body(boletoService.findAll());
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<Boleto>> findUserBoleto(){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findBoletoCharges());
     }
 }
